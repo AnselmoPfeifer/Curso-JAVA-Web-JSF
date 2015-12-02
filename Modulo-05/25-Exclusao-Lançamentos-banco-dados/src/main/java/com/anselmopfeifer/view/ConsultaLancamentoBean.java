4563776c379d2a5be0ab78113b30b371b40eef22
Anselmo Pfeifer <anspfeifer@gmail.com>
2015-12-01 22:06:47 -0300

    Exclusão de lançamentos do banco de dados

--- Modulo-05/25-Exclusao-Lançamentos-banco-dados/src/main/java/com/anselmopfeifer/view/ConsultaLancamentoBean.java
+++ Modulo-05/25-Exclusao-Lançamentos-banco-dados/src/main/java/com/anselmopfeifer/view/ConsultaLancamentoBean.java
@@ -5,18 +5,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Order;
 
 import com.anselmopfeifer.model.Lancamento;
+import com.anselmopfeifer.util.FacesUtil;
 import com.anselmopfeifer.util.HibernateUtil;
 
 @ManagedBean
 public class ConsultaLancamentoBean implements Serializable {
 
 	private List<String> lancamentos = new ArrayList<String>();
+	private Lancamento lancamentoSelecionado;
+
+	public void excluir() {
+		if(this.lancamentoSelecionado.isPago()) {
+			FacesUtil.addMensagem(FacesMessage.SEVERITY_ERROR,
+					"Lançamento já foi pago e nao pode ser excluido!");
+		} else {
+			Session session = HibernateUtil.getSession();
+			Transaction trx = session.beginTransaction();
+			
+			session.delete(lancamentoSelecionado);
+			
+			trx.commit();
+			session.close();
+			
+			this.inicializar();
+			
+			FacesUtil.addMensagem(FacesMessage.SEVERITY_INFO,"Lançamento Excluido com Sucesso!");
+		}
+
+	}
 
 	@SuppressWarnings("unchecked")
 	@PostConstruct
@@ -28,6 +53,14 @@ public class ConsultaLancamentoBean implements Serializable {
 
 	public List<String> getLancamentos() {
 		return lancamentos;
+	}
+
+	public Lancamento getLancamentoSelecionado() {
+		return lancamentoSelecionado;
+	}
+
+	public void setLancamentoSelecionado(Lancamento lancamentoSelecionado) {
+		this.lancamentoSelecionado = lancamentoSelecionado;
 	}
 
 }
